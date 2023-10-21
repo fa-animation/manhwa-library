3a8f5ccd7605fb193cd7f4dff88c29fa6daa359a
KayoRonald <kayoronald4321@gmail.com>
2023-10-21 19:17:58 -0300

    feat: create migration

--- server/albemic/env.py
+++ server/albemic/env.py
@@ -1,5 +1,6 @@
 from logging.config import fileConfig
 from app.infra.core.database.connect import Base
+from app.infra.models.models import *
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
